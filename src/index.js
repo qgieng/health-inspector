be8d79a076f3d9f29f9ef11f1994c723b87c84ab
Quay Gieng <qgieng@dhcp-v093-083.mobile.uci.edu>
2017-10-09 20:02:16 -0700

    Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import SearchBar from './components/SearchBar';
 
+
+
+const App = () => {
+
+        return (
+            <div>
+                <SearchBar />
+            </div>
+
+        )
+}
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
